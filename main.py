dc18b9553144c1d068902576c749d877cad773f3
DampAdam <brand.adam@email.cz>
2024-01-21 23:49:20 +0100

    drobne zmeny

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ clr()
 
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
 import random as rnd
 
 clr()
 
-
-q = 150 # kvalita (max. 200)
-aug_per_image = 3 # počet augmentovaných obrázků na obrázek
-min_res_factor = 1 # minimální zvětšení/zmenšení obrázku (1 = 100% z kvality)
-
+res_scale = 0.75 # základní rozlišení obrázku (0.75 = 75% z originálního rozlišení)
+aug_per_image = 30 # počet augmentovaných obrázků na obrázek
+min_res_factor = 1 # minimální zvětšení/zmenšení obrázku (1 = 100% ze základního rozlišení)
 
 
-target_size = (4*q, 3*q)
-x = 4*q
-y = 3*q
-max_res_factor = 200/q
-a = max_res_factor-min_res_factor
+res_width = 1200    
+res_height = 1600
+
+
+x = res_scale*res_width
+y = res_scale*res_height
+a = 1/res_scale-min_res_factor
 
 datagen = ImageDataGenerator(
     rotation_range=40,
@@ -33,14 +32,10 @@ for root, _, files in os.walk("C:/Users/badam/Documents/RozpoznavaniTextuDataset
         if not file.endswith((".jpg", ".jpeg", ".png")):
             continue
 
-        r = rnd.random()*a+min_res_factor
-        img = image.img_to_array(image.load_img(os.path.join(root, file), target_size=(int(x*r), int(y*r))))
-        img = img.reshape((1,) + img.shape)
-
-        i = 0
-        for batch in datagen.flow(img, batch_size=1, save_to_dir="C:/Users/badam/Documents/RozpoznavaniTextuAugmented", save_prefix="aug", save_format="jpeg"):
-            i += 1
-            if i >= aug_per_image:
-                break
+        for i in range(aug_per_image):
+            r = rnd.random()*a+min_res_factor
+            img = image.img_to_array(image.load_img(os.path.join(root, file), target_size=(int(y*r), int(x*r))))
+            img = img.reshape((1,) + img.shape)
+            datagen.flow(img, batch_size=1, save_to_dir="C:/Users/badam/Documents/RozpoznavaniTextuAugmented", save_prefix="aug", save_format="jpeg").next()
 
 print("Augmentace dokončena.")
